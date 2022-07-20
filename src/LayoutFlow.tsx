1cb51f26b2a8c35698f04abce53266b03af236d1
Sepehr Memarzadeh <sepehr@artbees.net>
2022-07-20 12:37:51 +0430

    just code clean up and refactors

--- src/LayoutFlow.tsx
+++ src/LayoutFlow.tsx
@@ -6,72 +6,17 @@ import ReactFlow, {
   useStoreState,
   Node,
   useStoreActions,
-  Elements,
-  isEdge,
 } from 'react-flow-renderer';
-import dagre from 'dagre';
+import { getLayoutedElements } from './helpers/layouterByDagre';
 
-// base structure comes from https://reactflow.dev/examples/layouting/
-// worth taking a look: https://github.com/wbkd/react-flow/issues/5
-
-const getLayoutedElements = (elements: Elements<any>) => {
-  const dagreGraph = new dagre.graphlib.Graph();
-  dagreGraph.setDefaultEdgeLabel(() => ({}));
-  dagreGraph.setGraph({ rankdir: 'TB' }); // set direction to "Top-to-Bottom"
-
-  let rootId = '';
-
-  elements.forEach(el => {
-    if (isNode(el)) {
-      if (isRootNode(el, elements)) {
-        rootId = el.id;
-      }
-
-      dagreGraph.setNode(el.id, {
-        height: el.__rf.height,
-        width: Math.max(el.__rf.width, 120),
-      });
-    } else {
-      dagreGraph.setEdge(el.target, el.source); // we add the edges with reverse direction and then flip the graph later. This helps to have a better layout when we have final End(Exit) node in the graph.
-    }
-  });
-
-  dagre.layout(dagreGraph);
-
-  const rootPosition = dagreGraph.node(rootId);
-
-  return elements.map(el => {
-    if (isNode(el)) {
-      const nodeWithPosition = dagreGraph.node(el.id);
-      // el.targetPosition = isHorizontal ? 'left' : 'top';
-      // el.sourcePosition = isHorizontal ? 'right' : 'bottom';
-
-      // unfortunately we need this little hack to pass a slightly different position
-      // to notify react flow about the change. Moreover we are shifting the dagre node position
-      // (anchor=center center) to the top left so it matches the react flow node anchor point (top left).
-      // ---- NOTE: the 3-line comment above from the example itself doesn't seem relevant. We experienced
-      // ---- the issue that position didn't get updated and it got fixed by being immutable and creating a new object for the node.
-      return {
-        ...el,
-        position: {
-          // add position prop immutably
-          x: nodeWithPosition.x - el.__rf.width / 2 - rootPosition.x,
-          y: -(nodeWithPosition.y + el.__rf.height / 2 - rootPosition.y),
-        },
-      };
-    }
-
-    return el;
-  });
-};
+}
 
 const LayoutFlow = React.forwardRef(
   (rendererProps: ReactFlowProps, ref: React.Ref<HTMLDivElement>) => {
     const { elements } = rendererProps;
     const [layoutedElements, setLayoutedElements] = useState(elements); // initially just set to elements. we apply the layout later on when we have the width and height of each element.
     const [lastLayoutedNodes, setLastLayoutedNodes] = useState<Node[]>([]);
-    const [extent, setExtent] =
-      useState<ReactFlowProps['translateExtent']>(undefined); // used to limit the scrollable area.
+    const [extent, setExtent] = useState<Extent>(undefined); // Used to limit the scrollable area.
 
     useEffect(() => {
       setLastLayoutedNodes([]); // to enforce the re-layout at the other part
@@ -93,15 +38,10 @@ const LayoutFlow = React.forwardRef(
     }, [elements]);
 
     return (
-      // boxSizing: 'border-box' should be for all children. So maybe we should have some sort of css or style tag?
       <div
         ref={ref}
         className="artbees-flow__LayoutFlow"
-        style={{
-          flexGrow: 1,
-          width: '100%',
-          height: '100%',
-        }}
+        style={{ flexGrow: 1, width: '100%', height: '100%' }}
       >
         <style>{`.artbees-flow__LayoutFlow * {
           box-sizing: border-box;
@@ -178,17 +118,6 @@ function ReactFlowStateHandler({
     onNodesChange(nodes);
   }, [nodes]);
   return null;
-}
-
-function isRootNode(node: Node<any>, elements: Elements<any>) {
-  for (let el of elements) {
-    if (isEdge(el)) {
-      if (el.target === node.id) {
-        return false;
-      }
-    }
-  }
-  return true;
 }
 
 function shouldRelayout(newNodes: Node[], prevNodes: Node[]): Boolean {
